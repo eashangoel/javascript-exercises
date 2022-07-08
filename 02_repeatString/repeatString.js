f4e1f3c80631b8ff06e68265c6be56826d0891b4
Eashan Goel <eashan2@gmail.com>
2022-07-08 14:07:01 +0530

    Completed the first three javascript exercises, all jest cases passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    returnString= '';
+    if (num<0){
+        return 'ERROR';
+    }
+    for (let i=0; i<num; i++){
+            returnString = returnString+str;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(initialString) {
+   const initialArray = Array.from(initialString)
+   let finalArray = '';
+   for (let i=initialArray.length-1 ; i>=0 ; i--)
+   {
+        finalArray = finalArray.concat(initialArray[i]);
+   }
+   return finalArray;
 };
 
 // Do not edit below this line
